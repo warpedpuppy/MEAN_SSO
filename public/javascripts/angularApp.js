339437b00e9229327c4492a50e772a2bde8c757a
ted <ted@warpedpuppy.com>
2016-02-06 08:49:37 -0800

    initial commit

---
+++ public/javascripts/angularApp.js
@@ -0,0 +1,130 @@
+/**
+ * Created by edwardwalther on 2/4/16.
+ */
+
+var app = angular.module('authentiction_app', ['ui.router']);
+
+app.controller('MainCtrl', ['$scope', function($scope){
+
+
+    $scope.isLoggedIn = auth.isLoggedIn;
+
+
+}]);
+app.controller('NavCtrl', [
+    '$scope',
+    'auth',
+    function($scope, auth){
+        $scope.isLoggedIn = auth.isLoggedIn;
+        $scope.currentUser = auth.currentUser;
+        $scope.logOut = auth.logOut;
+    }]);
+
+app.controller('AuthCtrl', [
+        '$scope',
+        '$state',
+        'auth',
+        function($scope, $state, auth){
+            $scope.user = {};
+
+            $scope.register = function(){
+                auth.register($scope.user).error(function(error){
+                    $scope.error = error;
+                }).then(function(){
+                    $state.go('home');
+                });
+            };
+
+            $scope.logIn = function(){
+                auth.logIn($scope.user).error(function(error){
+                    $scope.error = error;
+                }).then(function(){
+                    $state.go('home');
+                });
+            };
+        }])
+
+app.factory('auth', ['$http', '$window', function($http, $window){
+    var auth = {};
+    auth.saveToken = function (token){
+        $window.localStorage['generic-auth-token'] = token;
+    };
+
+    auth.getToken = function (){
+        return $window.localStorage['generic-auth-token'];
+    }
+    auth.isLoggedIn = function(){
+        var token = auth.getToken();
+
+        if(token){
+            var payload = JSON.parse($window.atob(token.split('.')[1]));
+
+            return payload.exp > Date.now() / 1000;
+        } else {
+            return false;
+        }
+    };
+    auth.currentUser = function(){
+        if(auth.isLoggedIn()){
+            var token = auth.getToken();
+            var payload = JSON.parse($window.atob(token.split('.')[1]));
+
+            return payload.username;
+        }
+    };
+    auth.register = function(user){
+        return $http.post('/register', user).success(function(data){
+            auth.saveToken(data.token);
+        });
+    };
+    auth.logIn = function(user){
+        return $http.post('/login', user).success(function(data){
+            auth.saveToken(data.token);
+        });
+    };
+    auth.logOut = function(){
+        $window.localStorage.removeItem('generic-auth-token');
+    };
+    return auth;
+}]);
+
+
+app.config([
+    '$stateProvider',
+    '$urlRouterProvider',
+    function($stateProvider, $urlRouterProvider) {
+
+        $stateProvider
+            .state('home', {
+                url: '/home',
+                templateUrl: '/home.html',
+                controller: 'MainCtrl',
+                resolve: {
+                    postPromise: ['posts', function(posts){
+                        return posts.getAll();
+                    }]
+                }
+            });
+        $stateProvider.state('login', {
+            url: '/login',
+            templateUrl: '/login.html',
+            controller: 'AuthCtrl',
+            onEnter: ['$state', 'auth', function($state, auth){
+                if(auth.isLoggedIn()){
+                    $state.go('home');
+                }
+            }]
+        })
+        $stateProvider.state('register', {
+                url: '/register',
+                templateUrl: '/register.html',
+                controller: 'AuthCtrl',
+                onEnter: ['$state', 'auth', function($state, auth){
+                    if(auth.isLoggedIn()){
+                        $state.go('home');
+                    }
+                }]
+            });
+
+        $urlRouterProvider.otherwise('home');
+    }]);
